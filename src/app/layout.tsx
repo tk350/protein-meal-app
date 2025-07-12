bbc3a183dc161cc12f60b2671cf0d6e0e7f685eb
tk350 <218123390+tk350@users.noreply.github.com>
2025-07-12 23:48:32 +0900

    Fix AdSense script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+<head>
+  <script
+    async
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2990244647750003"
+    crossOrigin="anonymous">
+  </script>
+</head>
+
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <Navbar />
         <main className="min-h-screen px-4">{children}</main>
@@ -35,3 +43,4 @@ export default function RootLayout({
     </html>
   );
 }
+
